2e018c517f2563605071880bbd01ddd83e097832
Devin <devin@blackhat.co.za>
2023-05-05 11:52:26 +0200

    feat: added GitHub nav Link
    * ci: update lock file
    
    * feat: added github nav link
    
    * docs: update to readme
    
    * ci: removed unused ci files
    
    * bug: fixed param names in logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,8 +69,8 @@ export const Header = forwardRef(function Header({ className }, ref) {
         <nav className="hidden md:block">
           <ul role="list" className="flex items-center gap-8">
             <TopLevelNavItem href="/">API</TopLevelNavItem>
-            <TopLevelNavItem href="#">Documentation</TopLevelNavItem>
-            <TopLevelNavItem href="#">Support</TopLevelNavItem>
+            <TopLevelNavItem href="/">Documentation</TopLevelNavItem>
+            <TopLevelNavItem href="https://github.com/walletd/walletd">GitHub</TopLevelNavItem>
           </ul>
         </nav>
         <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
@@ -78,9 +78,6 @@ export const Header = forwardRef(function Header({ className }, ref) {
           <MobileSearch />
           <ModeToggle />
         </div>
-        {/* <div className="hidden min-[416px]:contents">
-          <Button href="#">Sign in</Button>
-        </div> */}
       </div>
     </motion.div>
   )
